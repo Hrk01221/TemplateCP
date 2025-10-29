269c1699c904b4e1f5ac29e314d97d30f17ff43e
Hridoy Nandi <133546308+Hrk01221@users.noreply.github.com>
2025-10-30 02:38:53 +0600

    Refactor Template.cpp with debugging support

--- Template.cpp
+++ Template.cpp
@@ -1,5 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
+#ifndef ONLINE_JUDGE
+#include "debug.hpp"
+#else
+#define debug(x...)
+#endif
 #define int long long
 #define endl "\n"
 #define ff first
@@ -7,17 +12,18 @@ using namespace std;
 #define pb push_back
 #define all(a) a.begin(),a.end()
 #define rall(a) a.rbegin(),a.rend()
+#define sz(a) a.size()
 #define pii pair<int,int>
-#define Fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-const int MOD = 1000000007;
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+const int MOD = 1e9 + 7;
 const int INF = 1e18;
 const int N = 2e5;
-void solve(int tc){}
-int32_t main(){
-    Fast
-    int t=1;
+void solve(int tc) {}
+int32_t main() {
+    FAST
+    int t = 1;
     cin >> t;
-    for(int tc=1;tc<=t;tc++)
+    for (int tc = 1; tc <= t; tc++)
         solve(tc);
     return 0;
 }
