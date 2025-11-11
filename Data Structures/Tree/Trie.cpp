1194bcbc0dd5699f02b46d691a20f1814bc95a63
Hridoy Nandi <133546308+Hrk01221@users.noreply.github.com>
2025-11-12 01:28:03 +0600

    Refactor Trie structure and node definitions

--- Data Structures/Tree/Trie.cpp
+++ Data Structures/Tree/Trie.cpp
@@ -1,62 +1,58 @@
+const int SZ = 26;
+char INIT = 'a';
 struct Trie { //init->Trie T
-    struct node {
+    struct Node {
         int wordCnt = 0, cnt = 0;
-        node* next[26] = {};
-        ~node() {
-            for(int i=0;i<26;i++)
-                if(next[i])
-                    delete(next[i]);
-        }
+        Node* next[SZ] = {};
     };
-    node* root;
-    Trie(){root = new node();}
-    ~Trie(){delete root;}
+    Node* root;
+    Trie(){root = new Node();}
     void insert(const string &s) {
-        node* cur = root;
+        Node* curr = root;
         for (char c : s) {
-            int id = c - 'a';
-            if (!cur->next[id])
-                cur->next[id] = new node();
-            cur = cur->next[id];
-            cur->cnt++;
+            int id = c - INIT;
+            if (!curr->next[id])
+                curr->next[id] = new Node();
+            curr = curr->next[id];
+            curr->cnt++;
         }
-        cur->wordCnt++;
+        curr->wordCnt++;
     }
     void erase(const string &s) {
         if(!search(s))return;
-        node* cur = root;
+        Node* curr = root;
         for (char c : s) {
-            int id = c - 'a';
-            cur = cur->next[id];
-            cur->cnt--;
+            int id = c - INIT;
+            curr = curr->next[id];
+            curr->cnt--;
         }
-        cur->wordCnt--;
+        curr->wordCnt--;
     }
     bool search(const string &s) {
-        node* cur = root;
+        Node* curr = root;
         for (char c : s) {
-            int id = c - 'a';
-            if (!cur->next[id]) return false;
-            cur = cur->next[id];
+            int id = c - INIT;
+            if (!curr->next[id]) return false;
+            curr = curr->next[id];
         }
-        return cur->wordCnt > 0;
+        return curr->wordCnt > 0;
     }
     int countPrefix(const string &p) {
-        node* cur = root;
+        Node* curr = root;
         for (char c : p) {
-            int id = c - 'a';
-            if (!cur->next[id]) return 0;
-            cur = cur->next[id];
+            int id = c - INIT;
+            if (!curr->next[id]) return 0;
+            curr = curr->next[id];
         }
-        return cur->cnt;
+        return curr->cnt;
     }
     string longestPrefix(const string &s) {
-        node* cur = root;
+        Node* curr = root;
         string res = "";
         for (char c : s) {
-            int id = c - 'a';
-            if (!cur->next[id]) break;
-            cur = cur->next[id];
+            int id = c - INIT;
+            if (!curr->next[id]) break;
+            curr = curr->next[id];
             res.push_back(c);
         }
         return res;
