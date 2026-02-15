9eb27340f5625b85772fafb2c46893eb0aa97679
Hridoy Nandi <133546308+Hrk01221@users.noreply.github.com>
2026-02-15 11:47:20 +0600

    Refactor DNC algorithm and add compute function

--- dp/DNC.cpp
+++ dp/DNC.cpp
@@ -1,20 +1,34 @@
-void dnc(int k,int l,int r,int optl,int optr){
-    if(l > r) return;
-    int mid = (l+r) >> 1;
-    int optm = optl;
-    for(int i=optl;i<=min(mid,optr);i++){
-        int ret = (i?dp[k-1][i-1]:0) +  cost(i,mid-1,a,b,x[mid]);
-        if(ret < dp[k][mid]){
-            dp[k][mid] = ret;
-            optm = i;
+long long cost(int i, int j, vector<long long>&pref) {
+    if (j < i)return INF;
+    long long sum = pref[j] - pref[i - 1];
+    sum = (sum * (sum + 1)) / 2;
+    return sum;
+}
+void compute(int groupNo , int L , int R, int optL , int optR , vector<vector<long long>>&dp , vector<long long>&pref) {
+    if (L > R)return;
+    int mid = (L + R) >> 1;
+    int id = groupNo;
+    dp[id][mid] = INF;
+    int optNow = optL;
+    for (int j = optL; j <= optR && j < mid; j++) {
+        long long ret = dp[id - 1][j] + cost(j + 1 , mid , pref);
+        if (ret < dp[id][mid]) {
+            dp[id][mid] = ret;
+            optNow = j;
         }
     }
-    dnc(k,l,mid-1,optl,optm);  
-    dnc(k,mid+1,r,optm,optr);
+    cout << mid << " " << dp[id][mid] << endl;
+    compute(groupNo , L , mid - 1 , optL , optNow , dp , pref);
+    compute(groupNo , mid + 1 , R , optNow , optR , dp , pref);
 }
-void solve(int tc){
-    //init base case carefully
+long long minPartitionScore(vector<int>&nums, int k) {
+    int n = nums.size();
+    vector<long long>v;
+    v.push_back(0);
+    for (auto it : nums) v.push_back(it);
+    for (int i = 1; i <= n; i++)v[i] += v[i - 1];
+    vector<vector<long long>>dp(k + 1 , vector<long long>(n + 1, INF));
     dp[0][0] = 0;
-    for(int i=1;i<=k;i++)dnc(i,1,n,1,n);
-    cout << dp[k][n] << endl;
+    for (int i = 1; i <= k; i++)compute(i , 1 , n , 0 , n - 1 , dp , v);
+    return dp[k][n];
 }
