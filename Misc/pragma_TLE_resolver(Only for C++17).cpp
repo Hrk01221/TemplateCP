92e615e3a7ce71d769f87a5589c44c36364f238a
Hridoy Nandi <133546308+Hrk01221@users.noreply.github.com>
2026-02-20 15:43:17 +0600

    Create pragma_TLE_resolver(Only for C++17).cpp

---
+++ Misc/pragma_TLE_resolver(Only for C++17).cpp
@@ -0,0 +1,6 @@
+#pragma GCC diagnostic ignored "-Wc++17-extensions"
+#pragma GCC diagnostic ignored "-Wconversion"
+#pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("inline")
+#pragma GCC optimize("unroll-loops")
